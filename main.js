f680adb75c29706ce9b7ed9034de8314c5959e63
SoulRiaper <soulriaper@gmail.com>
2024-02-27 15:02:19 +0300

    refactor main.js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import ContractNotifier from "./src/ContractNotifier.js";
 import options from "./conf/options.js"
 
-const app = new ContractNotifier(options);
-await app.init();
-console.log(await app.getPersonToNotify("d:hi0hb9q1zvafkbv4ice02p6n86"));
+async function run() {
+    const app = new ContractNotifier(options);
+    await app.init();
+    const contractTestUri = "d:hi0hb9q1zvafkbv4ice02p6n86";
+    const personsToNotify = await app.getPersonToNotify(contractTestUri);
+    await app.sendMails(personsToNotify, [contractTestUri]);
+}
+
+run();
