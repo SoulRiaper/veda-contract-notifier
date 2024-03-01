7be7d0272921c7f1e17665dc384672fe94fc6659
SoulRiaper <soulriaper@gmail.com>
2024-03-01 09:20:44 +0300

    Logging into log folder

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 time=`date -Iseconds`
 echo $time
-/sbin/start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/.pid --background --startas /bin/bash -- -c "exec node --use-openssl-ca --openssl-legacy-provider main.js >> $PWD/${PWD##*/}-$time.log 2>&1"
+/sbin/start-stop-daemon --start --verbose --chdir $PWD --make-pidfile --pidfile $PWD/.pid --background --startas /bin/bash -- -c "exec node --use-openssl-ca --openssl-legacy-provider main.js >> log/${PWD##*/}-$time.log 2>&1"
