05423a0daeeeb7cc7af09d3238c8e5223485e0ab
Roman Karpov <roman.karpov@gmail.com>
2024-03-06 19:10:45 +0300

    fix: rename main.js to veda-contract-notifier.js

---
+++ src/veda-contract-notifier.js
@@ -0,0 +1,28 @@
+import ContractNotifier from "./ContractNotifier.js";
+import options from "../conf/options.js"
+import log from "./log.js"
+
+async function run() {
+    log.info("Script starting.");
+    const app = new ContractNotifier(options);
+    await app.init();
+    
+    const contractsQueryResult = await app.getContractsFromStoredQuery();
+
+    const toSendList = await app.getToSendList(contractsQueryResult.result);
+    for (let toSendPerson in toSendList) {
+        if (!toSendPerson) {
+            continue;
+        }
+        try {
+            await app.sendMail(toSendPerson, toSendList[toSendPerson]);
+        } catch (error) {
+            log.error(`Cant send email for: ${toSendPerson}.`);
+            log.error(error.message);
+        }
+    }
+    log.info("Script end work.");
+}
+
+await run();
+process.exit(0);
