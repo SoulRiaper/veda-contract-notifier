c4024b9ea8a5b15af339b69c42e6418108441341
SoulRiaper <soulriaper@gmail.com>
2024-03-11 09:00:06 +0300

    fix: code style + try catch handling

--- src/ContractNotifier.js
+++ src/ContractNotifier.js
@@ -1,101 +1,130 @@
-import VedaService from "./VedaService.js";
-import { BaseModel } from "veda-client";
-import Mustache from "mustache";
-import log from "./log.js"
+import VedaService from './VedaService.js';
+import {BaseModel} from 'veda-client';
+import Mustache from 'mustache';
+import log from './log.js';
 
 export default class ContractNotifier {
+  constructor (options) {
+    this.options = options;
+  }
 
-    constructor (options) {
-        this.options = options;
+  async init () {
+    this.veda = new VedaService(this.options);
+    await this.veda.init();
+  }
+
+  async getContractsFromStoredQuery () {
+    try {
+      return await this.veda.getDocsFromStoredQuery();
+    } catch (error) {
+      log.error(`Failed to query data: ${error.message}`);
+    }
+  }
+
+  async getPersonToNotify (contractUri) {
+    const contract = new BaseModel(contractUri);
+    try {
+      await contract.load();
+    } catch (error) {
+      log.error(contractUri, error.message);
+      throw error;
     }
 
-    async init () {
-        this.veda = new VedaService(this.options);
-        await this.veda.init();
-    }
+    const isExecutorValid = await this.isContractResponsibleValid(contract, 'mnd-s:executorSpecialistOfContract')
+      .catch((error) => {
+        log.error(`Failed to check isExecutorValid: ${error.message}`);
+        return false;
+      });
+    const isSupporterValid = await this.isContractResponsibleValid(contract, 'mnd-s:supportSpecialistOfContract')
+      .catch((error) => {
+        log.error(`Failed to check isSupporterValid: ${error.message}`);
+        return false;
+      });
+    const isManagerValid = await this.isContractResponsibleValid(contract, 'mnd-s:ContractManager')
+      .catch((error) => {
+        log.error(`Failed to check isManagerValid: ${error.message}`);
+        return false;
+      });
+    const isDepValid = await this.isContractResponsibleValid(contract, 'v-s:responsibleDepartment')
+      .catch((error) => {
+        log.error(`Failed to check isDepValid: ${error.message}`);
+        return false;
+      });
 
-    async getContractsFromStoredQuery () {
-        return await this.veda.getDocsFromStoredQuery();
-    }
-
-    async getPersonToNotify (contractUri) {
-        const contract = new BaseModel(contractUri);
+    if (!isExecutorValid) {
+      if (contract.hasValue('v-s:responsibleDepartment')) {
         try {
-            await contract.load();
+          const responsibleDep = contract['v-s:responsibleDepartment'][0];
+          await responsibleDep.load();
+          const depChief = await this.veda.getChiefUri(responsibleDep);
+          if (depChief) {
+            const depChiefObj = new BaseModel(depChief);
+            if (await this.veda.isIndividValid(depChiefObj)) {
+              return depChief;
+            }
+          }
         } catch (error) {
-            log.error(contractUri, error.message);
-            throw error;
+          log.error(`Failed to handle responsibleDep: ${error.message}`);
         }
-        const isExecutorValid = await this.isContractResponsibleValid(contract, "mnd-s:executorSpecialistOfContract");
-        const isSupporterValid = await this.isContractResponsibleValid(contract, "mnd-s:supportSpecialistOfContract");
-        const isManagerValid = await this.isContractResponsibleValid(contract, "mnd-s:ContractManager");
-        const isDepValid = await this.isContractResponsibleValid(contract, "v-s:responsibleDepartment");
+      }
+      return 'd:contract_controller_role';
+    }
+    if (!isSupporterValid || !isManagerValid || !isDepValid) {
+      return contract['mnd-s:executorSpecialistOfContract'][0].id;
+    }
+    return 'd:contract_controller_role';
+  }
 
-        if (!isExecutorValid) {
-            if (contract.hasValue("v-s:responsibleDepartment")) {
-                const responsibleDep = contract["v-s:responsibleDepartment"][0];
-                await responsibleDep.load();
-                const depChief = await this.veda.getChiefUri(responsibleDep);
-                if (depChief) {
-                    const depChiefObj = new BaseModel(depChief);
-                    if (await this.veda.isIndividValid(depChiefObj)) {
-                        return depChief;
-                    }
-                }
-            }
-            return "d:contract_controller_role";
+  async isContractResponsibleValid (contract, responsibleProp) {
+    try {
+      if (contract.hasValue(responsibleProp)) {
+        const responsible = contract[responsibleProp][0];
+        await responsible.load();
+        return await this.veda.isIndividValid(responsible);
+      }
+      return false;
+    } catch (error) {
+      console.error(`Failed to validate contract responsible: ${error}`);
+      throw error;
+    }
+  }
+
+  async getToSendList (contractsUri) {
+    const toSend = {};
+    for (let i = 0; i < contractsUri.length; i++) {
+      log.info(`Try to get responsible for contract: ${contractsUri[i]}`);
+      try {
+        const personToNotify = await this.getPersonToNotify(contractsUri[i]);
+        if (!toSend[personToNotify]) {
+          toSend[personToNotify] = [contractsUri[i]];
+        } else {
+          toSend[personToNotify].push(contractsUri[i]);
         }
-        if (!isSupporterValid || !isManagerValid || !isDepValid) {
-            return contract["mnd-s:executorSpecialistOfContract"][0].id;
-        }
-        return "d:contract_controller_role";
+      } catch (error) {
+        log.error(`Cant calculate person to notify CONTRACT: ${contractsUri[i]}`);
+        continue;
+      }
+      log.info(`Get responsible for: ${contractsUri[i]}`);
+    }
+    return toSend;
+  }
+
+  async sendMail (recipient, contractList) {
+    const view = {
+      app_name: this.veda.getAppName(),
+      contract_list: contractList.map((item) => this.options.veda.server + '#/' + item).join('\n'),
+    };
+    const letter = await this.veda.getMailLetterView(this.options.veda.mail.template);
+    letter.subject = Mustache.render(letter.subject, view).replace(/&#x2F;/g, '/');
+    letter.body = Mustache.render(letter.body, view).replace(/&#x2F;/g, '/');
+
+    const recipientObj = new BaseModel(recipient);
+    if (! await this.veda.isIndividValid(recipientObj)) {
+      recipient = 'd:contract_controller_role';
     }
 
-    async isContractResponsibleValid (contract, responsibleProp) {
-        if (contract.hasValue(responsibleProp)) {
-            const responsible = contract[responsibleProp][0];
-            await responsible.load();
-            return await this.veda.isIndividValid(responsible);
-        }
-        return await false;
-    }
-
-    async getToSendList (contractsUri) {
-        let toSend = {};
-        for(let i = 0; i < contractsUri.length; i++){
-            log.info(`Try to get responsible for contract: ${contractsUri[i]}`);
-            try {
-                const personToNotify = await this.getPersonToNotify(contractsUri[i]);
-                if (!toSend[personToNotify]) {
-                    toSend[personToNotify] = [contractsUri[i]];
-                } else {
-                    toSend[personToNotify].push(contractsUri[i]);
-                }
-            } catch (error) {
-                log.error(`Cant calculate person to notify CONTRACT: ${contractsUri[i]}`);
-                continue;
-            }
-            log.info(`Get responsible for: ${contractsUri[i]}`);
-        }
-        return toSend;
-    }
-
-    async sendMail (recipient, contractList) {
-        const view = {
-            app_name: this.veda.getAppName(),
-            contract_list: contractList.map(item => this.options.veda.server + "#/" + item).join('\n')
-        }
-        let letter = await this.veda.getMailLetterView(this.options.veda.mail.template);
-        letter.subject = Mustache.render(letter.subject, view).replace (/&#x2F;/g, '/');
-        letter.body = Mustache.render(letter.body, view).replace (/&#x2F;/g, '/');
-
-        const recipientObj = new BaseModel(recipient);
-        if (! await this.veda.isIndividValid(recipientObj)) {
-            recipient = "d:contract_controller_role";
-        }
-
-        const mailObj = this.veda.prepareEmailLetter(recipient, letter);
-        await mailObj.save();
-        log.info(`Mail send to: ${recipient}. Email obj uri: ${mailObj.id}`);
-    }
+    const mailObj = this.veda.prepareEmailLetter(recipient, letter);
+    await mailObj.save();
+    log.info(`Mail send to: ${recipient}. Email obj uri: ${mailObj.id}`);
+  }
 }
--- src/VedaService.js
+++ src/VedaService.js
@@ -1,49 +1,48 @@
-import { Backend, BaseModel, Model } from "veda-client";
-import log from "./log.js"
+import {Backend, BaseModel} from 'veda-client';
+import log from './log.js';
 
 export default class VedaService {
+  constructor (options) {
+    this.options = options;
+  }
 
-    constructor (options) {
-        this.options = options;
+  async init () {
+    try {
+      Backend.init(this.options.veda.server);
+      const authInfo = await this.authenticate();
+      log.info('Veda user authentication success:', this.options.veda.user);
+      log.info('Ticket will expire at:', new Date(authInfo.expires).toISOString());
+      setInterval(this.#refreshTicket.bind(this), (authInfo.expires - Date.now()) * 0.9);
+    } catch (error) {
+      log.error('Veda user authentication error:', error.message);
+      log.debug(error);
+      throw error;
     }
+  }
 
-    async init () {
-        try{
-            Backend.init(this.options.veda.server);
-            const authInfo = await this.authenticate();
-            log.info('Veda user authentication success:', this.options.veda.user);
-            log.info('Ticket will expire at:', new Date(authInfo.expires).toISOString());
-            setInterval(this.#refreshTicket.bind(this), (authInfo.expires - Date.now()) * 0.9);
-        } catch (error) {
-            log.error('Veda user authentication error:', error.message);
-            log.debug(error);
-            throw error;
-        }
+  async #refreshTicket () {
+    try {
+      const authInfo = await this.authenticate();
+      log.info('Veda ticket refreshed for user:', this.options.veda.user);
+      log.info('Ticket will expire at:', new Date(authInfo.expires).toISOString());
+    } catch (error) {
+      console.error('Veda ticket refresh error:', error.message);
+      log.error(error);
+      await timeout();
+      await this.#refreshTicket();
     }
+  }
 
-    async #refreshTicket () {
-        try {
-            const authInfo = await this.authenticate();
-            log.info('Veda ticket refreshed for user:', this.options.veda.user);
-            log.info('Ticket will expire at:', new Date(authInfo.expires).toISOString());
-        } catch (error) {
-            console.error('Veda ticket refresh error:', error.message);
-            log.error(error);
-            await timeout();
-            await this.#refreshTicket();
-        }
-    }
+  async authenticate () {
+    return await Backend.authenticate(this.options.veda.user, this.options.veda.password);
+  }
 
-    async authenticate () {
-        return await Backend.authenticate(this.options.veda.user, this.options.veda.password);
-    }
-
-    async getDocsFromStoredQuery () {
-        return await Backend.query({
-            from: 0,
-            top: 10000,
-            limit: 10000,
-            sql: `
+  async getDocsFromStoredQuery () {
+    return await Backend.query({
+      from: 0,
+      top: 10000,
+      limit: 10000,
+      sql: `
               select distinct t1.id
               from veda_tt."mnd-s:Contract" t1 final
                 left join veda_tt."v-s:Appointment" t2 final on t2.id=t1.mnd_s_ContractManager_str[1]
@@ -55,62 +54,61 @@ export default class VedaService {
                and t1.v_s_deleted_int = [0]
                and (t2.v_s_deleted_int=[1] or t3.v_s_deleted_int=[1] or t4.v_s_deleted_int=[1] or t5.v_s_deleted_int=[1])
               `,
-          });
-    }
+    });
+  }
 
-    async getChiefUri (department,depth) {
-        depth = depth || 0;
-        if ( department ) {
-            if ( depth > 15 ) return undefined;
-            if ( department["v-s:hasChief"] ) return department["v-s:hasChief"][0].id;
-            if ( department["v-s:parentUnit"] ) {
-            const dep = department['v-s:parentUnit'][0]
-            dep.load();
-            return this.getChiefUri(dep, depth + 1);
-            };
-        };
-        return undefined;
-    }
+  async getChiefUri (department, depth) {
+    depth = depth || 0;
+    if ( department ) {
+      if ( depth > 15 ) return undefined;
+      if ( department['v-s:hasChief'] ) return department['v-s:hasChief'][0].id;
+      if ( department['v-s:parentUnit'] ) {
+        const dep = department['v-s:parentUnit'][0];
+        dep.load();
+        return this.getChiefUri(dep, depth + 1);
+      };
+    };
+    return undefined;
+  }
 
-    async isIndividValid (individ) {
-        await individ.load();
-        if (individ.hasValue("v-s:valid") && individ.hasValue('v-s:deleted')) {
-            return individ.hasValue("v-s:valid", true) && individ.hasValue('v-s:deleted', false);
-        }
-        if (individ.hasValue("v-s:valid")) {
-            return individ.hasValue("v-s:valid", true);
-        }
-        if (individ.hasValue('v-s:deleted')) {
-            return individ.hasValue('v-s:deleted', false);
-        }
-        return true;
+  async isIndividValid (individ) {
+    await individ.load();
+    if (individ.hasValue('v-s:valid') && individ.hasValue('v-s:deleted')) {
+      return individ.hasValue('v-s:valid', true) && individ.hasValue('v-s:deleted', false);
     }
+    if (individ.hasValue('v-s:valid')) {
+      return individ.hasValue('v-s:valid', true);
+    }
+    if (individ.hasValue('v-s:deleted')) {
+      return individ.hasValue('v-s:deleted', false);
+    }
+    return true;
+  }
 
-    async getMailLetterView (templateUri) {
-        let result = {};
-        const mailTemplateObj = await Backend.get_individual(templateUri);
-        result["subject"] = mailTemplateObj["v-s:notificationSubject"][0].data;
-        result["body"] = mailTemplateObj["v-s:notificationBody"][0].data;
-        return result;
-    }
-    
-    prepareEmailLetter (recipient, letterView) {
-        const letter = new BaseModel();
-        letter.addValue('rdf:type', "v-s:Email");
-        letter.addValue("v-wf:to", recipient);
-        letter.addValue("v-wf:from", this.options.veda.mail.senderAppointment);
-        letter.addValue("v-s:subject", letterView.subject);
-        letter.addValue("v-s:messageBody", letterView.body);
-        letter.addValue("v-s:hasMessageType", "v-s:OtherNotification");
-        letter.addValue("v-s:origin", "contract-notifier");
-        letter.addValue("v-s:created", new Date());
-        letter.addValue("v-s:creator", this.options.veda.mail.senderAppointment);
-        return letter;
-    }
+  async getMailLetterView (templateUri) {
+    const result = {};
+    const mailTemplateObj = await Backend.get_individual(templateUri);
+    result['subject'] = mailTemplateObj['v-s:notificationSubject'][0].data;
+    result['body'] = mailTemplateObj['v-s:notificationBody'][0].data;
+    return result;
+  }
 
-    #appName = "Optiflow";
-    getAppName () {
-      return this.#appName;
-    }
-    
+  prepareEmailLetter (recipient, letterView) {
+    const letter = new BaseModel();
+    letter.addValue('rdf:type', 'v-s:Email');
+    letter.addValue('v-wf:to', recipient);
+    letter.addValue('v-wf:from', this.options.veda.mail.senderAppointment);
+    letter.addValue('v-s:subject', letterView.subject);
+    letter.addValue('v-s:messageBody', letterView.body);
+    letter.addValue('v-s:hasMessageType', 'v-s:OtherNotification');
+    letter.addValue('v-s:origin', 'contract-notifier');
+    letter.addValue('v-s:created', new Date());
+    letter.addValue('v-s:creator', this.options.veda.mail.senderAppointment);
+    return letter;
+  }
+
+  #appName = 'Optiflow';
+  getAppName () {
+    return this.#appName;
+  }
 }
