850f31521322bf1cc7b9ca0644b8817cb965fc5c
SoulRiaper <soulriaper@gmail.com>
2024-03-14 15:33:19 +0300

    add: telegram service status notification

--- src/veda-contract-notifier.js
+++ src/veda-contract-notifier.js
@@ -3,13 +3,21 @@ import options from '../conf/options.js';
 import log from './log.js';
 
 async function run () {
-  log.info('Script starting.');
   const app = new ContractNotifier(options);
-  await app.init();
+  try {
+    await app.init();
+    log.info('Service started.');
+    await sendTelegram('🟢 Service started');
+  } catch (error) {
+    log.error('Service start error:', error.message);
+    await sendTelegram('🔴 Service start error:', error.message);
+    throw error;
+  }
 
   const contractsQueryResult = await app.getContractsFromStoredQuery()
-    .catch( (error) => {
+    .catch( async (error) => {
       log.error(`Critical query error: ${error.message}. Exit.`);
+      await sendTelegram('🔴 Service critical query error:', error.message);
     });
 
   const toSendList = await app.getToSendList(contractsQueryResult.result);
@@ -22,9 +30,11 @@ async function run () {
     } catch (error) {
       log.error(`Cant send email for: ${toSendPerson}.`);
       log.error(error.message);
+      await sendTelegram(`🟠 Service error: cant send email for: ${toSendPerson}. Error message: ${error.message}`);
     }
   }
   log.info('Script end work.');
+  await sendTelegram('🔴 Service stopped');
 }
 
 await run();
